42d1a804f80d457e05b54c90db38ea4c9497fff1
Gagandeep Singh <gdp.1807@gmail.com>
2021-11-15 20:56:44 +0530

    fixed

--- integration_tests/intrinsics_28.f90
+++ integration_tests/intrinsics_28.f90
@@ -1,12 +1,7 @@
 program debug
     implicit none
-    integer :: a, b, c, d, e, f, g, h
-    a = 10
-    b = -20
-    c = 30
-    d = -40
-    e = 0
-    f = 50
+    integer, parameter :: a = 10, b = -20, c = 30
+    integer, parameter :: d = -40, e = 0, f = 50
     print *, min(a, b, c, d, e, f)
     print *, max(a, b, c, d, e, f)
     print *, max(a, b)
