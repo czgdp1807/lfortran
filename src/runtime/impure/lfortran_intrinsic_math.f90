97b3248ef4e9112506a775f945fc0d698a2be237
Gagandeep Singh <gdp.1807@gmail.com>
2022-03-04 20:09:01 +0530

    Shifted data_and_time to m_string

--- src/runtime/impure/lfortran_intrinsic_math.f90
+++ src/runtime/impure/lfortran_intrinsic_math.f90
@@ -1166,9 +1166,4 @@ else
 end if
 end function
 
-subroutine date_and_time(date, time, zone, values)
-    character(len=*), intent(out), optional :: date, time, zone
-    integer, intent(out), optional :: values(8)
-end subroutine
-
 end module
