ef46507d6ac8fcef734de9bc642fd298c7f79968
Gagandeep Singh <gdp.1807@gmail.com>
2022-06-01 11:14:42 +0530

    Implemented ichar as Ichar node in ASR

--- src/libasr/asr_utils.h
+++ src/libasr/asr_utils.h
@@ -135,6 +135,7 @@ static inline ASR::ttype_t* expr_type(const ASR::expr_t *f)
         case ASR::exprType::ArrayMatMul: { return ((ASR::ArrayMatMul_t*)f)->m_type; }
         case ASR::exprType::ArrayPack: { return ((ASR::ArrayPack_t*)f)->m_type; }
         case ASR::exprType::Transfer: { return ((ASR::Transfer_t*)f)->m_type; }
+        case ASR::exprType::Ichar: { return ((ASR::Ichar_t*)f)->m_type; }
         default : throw LFortranException("Not implemented");
     }
 }
